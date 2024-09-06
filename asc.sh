67af8bd8e40d1aac157a8fe5c43430c8bdb2278b
zamzasalim <salimzamza@gmail.com>
2024-09-07 00:34:10 +0700

    Create asc.sh

---
+++ asc.sh
@@ -0,0 +1,8 @@
+echo -e "\033[0;35m"
+echo "====================================================="
+echo "                  AIRDROP ASC                        "
+echo "====================================================="
+echo -e '\e[35mTelegram Channel :\e[35m' @airdropasc
+echo -e '\e[35mTelegram Group :\e[35m' @autosultan_group
+echo "====================================================="
+echo -e "\e[0m"
